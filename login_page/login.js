9fdd8c189d3f897fbbae29da0dfba38bc3442b1f
DhruvaS_1BM21CS057 <103887139+1bm21cs057@users.noreply.github.com>
2023-06-29 22:17:51 +0530

    Updated login.js

--- login_page/login.js
+++ login_page/login.js
@@ -1,71 +1,68 @@
-const forms=document.querySelector(".forms"),
-    pwShowHide=document.querySelectorAll(".eye-icon"),
-      links=document.querySelectorAll(".link");
+const forms = document.querySelector(".forms"),
+  pwShowHide = document.querySelectorAll(".eye-icon"),
+  links = document.querySelectorAll(".link");
 
 let button = document.getElementById("button");
 
-pwShowHide.forEach(eyeIcon=>{
-    eyeIcon.addEventListener("click",()=>{
-        let pwFields=eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+pwShowHide.forEach(eyeIcon => {
+  eyeIcon.addEventListener("click", () => {
+    let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
 
-        pwFields.forEach(password=>{
-            if(password.type==="password"){
-                    password.type="text";
-                    eyeIcon.classList.replace("bx-hide","bx-show");
-                    return;
-            }
-            password.type="password";
-            eyeIcon.classList.replace("bx-show","bx-hide");
-
-        })
+    pwFields.forEach(password => {
+      if (password.type === "password") {
+        password.type = "text";
+        eyeIcon.classList.replace("bx-hide", "bx-show");
+        return;
+      }
+      password.type = "password";
+      eyeIcon.classList.replace("bx-show", "bx-hide");
     })
+  })
 })
-links.forEach(link=>{
-    link.addEventListener("click",e=>{
-        e.preventDefault();
-        forms.classList.toggle("show-signup");
 
-    })
+links.forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    forms.classList.toggle("show-signup");
+  })
 })
+
 var myInput = document.getElementById("psw");
-var cp=document.getElementById("cpsw");
+var cp = document.getElementById("cpsw");
 var letter = document.getElementById("letter");
 var capital = document.getElementById("capital");
 var number = document.getElementById("number");
 var length = document.getElementById("length");
 
+function checkPassword(event) {
+  event.preventDefault();
 
-function checkPassword(){
-  if(cp.value!=myInput.value)
-{
-  alert("Passwords don't match!");
-}
+  if (cp.value != myInput.value) {
+    alert("Passwords don't match!");
+  }
 }
 
-myInput.onfocus = function() {
+myInput.onfocus = function () {
   document.getElementById("message").style.display = "block";
 }
 
-
-myInput.onblur = function() {
+myInput.onblur = function () {
   document.getElementById("message").style.display = "none";
 }
 
-
-myInput.onkeyup = function() {
+myInput.onkeyup = function () {
   // Validate lowercase letters
   var lowerCaseLetters = /[a-z]/g;
-  if(myInput.value.match(lowerCaseLetters)) {
+  if (myInput.value.match(lowerCaseLetters)) {
     letter.classList.remove("invalid");
     letter.classList.add("valid");
   } else {
     letter.classList.remove("valid");
     letter.classList.add("invalid");
-}
-
+  }
 
   var upperCaseLetters = /[A-Z]/g;
-  if(myInput.value.match(upperCaseLetters)) {
+  if (myInput.value.match(upperCaseLetters)) {
     capital.classList.remove("invalid");
     capital.classList.add("valid");
   } else {
@@ -73,9 +70,8 @@ myInput.onkeyup = function() {
     capital.classList.add("invalid");
   }
 
- 
   var numbers = /[0-9]/g;
-  if(myInput.value.match(numbers)) {
+  if (myInput.value.match(numbers)) {
     number.classList.remove("invalid");
     number.classList.add("valid");
   } else {
@@ -83,8 +79,7 @@ myInput.onkeyup = function() {
     number.classList.add("invalid");
   }
 
- 
-  if(myInput.value.length >= 8) {
+  if (myInput.value.length >= 8) {
     length.classList.remove("invalid");
     length.classList.add("valid");
   } else {
@@ -93,6 +88,4 @@ myInput.onkeyup = function() {
   }
 }
 
-button.onclick=checkPassword();
-  
-
+button.onclick = checkPassword;
