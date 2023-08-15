a0827fc443f61604856489ce063db8d2d1ed7621
Gamana <gamana.cs21@bmsce.ac.in>
2023-08-15 18:52:34 +0530

    uploading homepage and index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+		$uri = 'https://';
+	} else {
+		$uri = 'http://';
+	}
+	$uri .= $_SERVER['HTTP_HOST'];
+	header('Location: '.$uri.'/homepage_(in_use)/webpage.html/');
+	exit;
+?>
+Something is wrong with the XAMPP installation :-(
